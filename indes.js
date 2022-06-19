585f2f89742e58da0d49967cace9377fe0ef3fff
SebaLalanne <103468093+SebaLalanne@users.noreply.github.com>
2022-06-19 18:16:58 -0300

    first commit

---
+++ indes.js
@@ -0,0 +1,23 @@
+/*let valor= parseInt( prompt("Ingrese el valor con el que quiere empezar"))
+for(let i = 1; i<= 10; i++ ){
+    let resultado= valor + i
+    alert (resultado)
+}
+*/
+/*
+for(let i = 1; i = 10; i++){
+    alert("Estes es el mensaje " + i)
+}
+*/
+let valor = parseInt( prompt("que valor?"))
+switch (valor){
+    case 1:
+        alert("valor 1")
+        break
+    case 2:
+        alert("Valor 2")
+        break
+    default:
+        alert ("What?")
+
+}
